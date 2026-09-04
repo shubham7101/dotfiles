9f4c602b2500163754f84788fb81ad4a59ab5799
Shubham Dhameliya <shubhamdhameliyaweb@gmail.com>
2026-09-04 12:27:03 +0530

    fixed nvim

--- .config/nvim/lua/shubham/plugins/treesitter.lua
+++ .config/nvim/lua/shubham/plugins/treesitter.lua
@@ -1,52 +1,37 @@
-return { -- Highlight, edit, and navigate code
+return {
   'nvim-treesitter/nvim-treesitter',
-  event = "BufReadPost",  -- Load only when a file is opened
+  lazy = false,
+  branch = "main",
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-  opts = {
-    ensure_installed = {
-      'go',
-      'lua',
-      'javascript',
-      'typescript',
-      'vimdoc',
-      'vim',
-      'regex',
-      'sql',
-      'dockerfile',
-      'toml',
-      'json',
-      'java',
-      'groovy',
-      'go',
-      'gitignore',
-      'graphql',
-      'yaml',
-      'make',
-      'cmake',
-      'markdown',
-      'markdown_inline',
-      'bash',
-      'tsx',
-      'css',
-      'html',
-    },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
-  },
-  -- There are additional nvim-treesitter modules that you can use to interact
-  -- with nvim-treesitter. You should go explore a few and see what interests you:
-  --
-  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  config = function()
+    local configs = require("nvim-treesitter")
+
+    configs.setup({
+      ensure_installed = {
+        'go', 'lua', 'javascript', 'typescript', 'vimdoc',
+        'vim', 'regex', 'sql', 'dockerfile', 'toml', 'json',
+        'java', 'groovy', 'gitignore', 'graphql', 'yaml',
+        'make', 'cmake', 'markdown', 'markdown_inline', 'bash',
+        'tsx', 'css', 'html',
+      },
+      auto_install = true,
+      highlight = {
+        enable = true, 
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
+    })
+
+    -- Automated fallback: Force-attaches highlighting on supported files automatically
+    vim.api.nvim_create_autocmd({ "FileType", "BufReadPost" }, {
+      group = vim.api.nvim_create_augroup("TSAutoStart", { clear = true }),
+      callback = function(args)
+        -- Skip special buffers and UI lists like netrw to avoid errors
+        if vim.bo[args.buf].buftype == "" and vim.bo[args.buf].filetype ~= "netrw" then
+          pcall(vim.treesitter.start)
+        end
+      end,
+    })
+  end,
 }
+
